f7372907fa5dbe7f24a904f4938224de3d82a803
Abdulaziz Musa <swiftv99@gmail.com>
2024-12-12 22:27:40 +0100

    Solve Missing Number and Max Product of Two Integers

---
+++ Array/2. Max Product of Two Integers.py
@@ -0,0 +1,20 @@
+"""
+Max Product of Two Integers
+Find the maximum product of two integers in an array where all elements are positive.
+
+Example
+
+arr = [1, 7, 3, 4, 9, 5] 
+max_product(arr) # Output: 63 (9*7)
+"""
+
+def max_product(arr):
+    max_val = second_max_val = 0
+    for elem in arr:
+        if elem > max_val and elem > second_max_val:
+            second_max_val = max_val
+            max_val = elem
+    return max_val * second_max_val
+
+arr = [1, 7, 3, 4, 9, 5] 
+print(max_product(arr)) # Output: 63 (9*7)
