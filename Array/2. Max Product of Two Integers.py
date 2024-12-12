91a971213946070b5c9aeeea0bf2081c638191c2
Abdulaziz Musa <swiftv99@gmail.com>
2024-12-12 22:55:49 +0100

    Update the solution for 2.Max Product of Two Integers

--- Array/2. Max Product of Two Integers.py
+++ Array/2. Max Product of Two Integers.py
@@ -10,10 +10,14 @@ max_product(arr) # Output: 63 (9*7)
 
 def max_product(arr):
     max_val = second_max_val = 0
+    
     for elem in arr:
-        if elem > max_val and elem > second_max_val:
+        if elem > max_val:
             second_max_val = max_val
             max_val = elem
+        elif elem > second_max_val:
+            second_max_val = elem
+            
     return max_val * second_max_val
 
 arr = [1, 7, 3, 4, 9, 5] 
